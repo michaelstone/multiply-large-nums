acfd569f49cd682d4bb5842bfa1973b715223357
michael stone <michaelstone.js@gmail.com>
2017-10-24 21:57:52 +0100

    Do some code clean up and removing of for loops.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Wrote this as a practice exercise after coming across Project Euler #1 on Hackerrack which seemed to be failing due to loss of precision in javascript with large numbers. Seems to be functional, but could use some more cleaning up.
+
 # TODO:
 
 - Add linter
--- src/index.js
+++ src/index.js
@@ -1,51 +1,44 @@
 
 function multiplyStr(multiplier, multiplicand) {
+
+    var lines = multiplicand.split('')
+        .reverse()
+        .map(x => parseInt(x))
+        .map((multiplicandDigit, index) => {
+            let nextLine = Array(index).fill(0);
+            let carry = 0;
     
-    var lines = [];
-    var multiplicandLen = multiplicand.length;
-    for (let i = 0; i < multiplicandLen; i++) {
-        let digit2 = getDigitFromStrAt(multiplicand, multiplicand.length - 1);
-        lines[i] = Array(i).fill(0);
-        let carry = 0;
+            multiplier.split('')
+                .reverse()
+                .map(x => parseInt(x))
+                .map((multiplierDigit) => {
+                    let productWithCarry = (multiplicandDigit * multiplierDigit) + carry;
+                    let splitNum = splitNumAtLeastSignificantDigit(productWithCarry);
+        
+                    // TODO: create data structure here for current calculation + carry to remove usage of carry side effect
+                    carry = splitNum[0];
+                    nextLine.unshift(splitNum[1]);
+                });
+    
+            if (carry !== 0) {
+                nextLine.unshift(carry);
+            }
 
-        let unprocessedMultiplier = multiplier;
-        for (let j = multiplier.length; j > 0; j--) {
-            let digit1 = getLastDigit(unprocessedMultiplier);
-            let productWithCarry = (digit1 * digit2) + carry;
-            let splitNum = splitNumAtLeastSignificantDigit(productWithCarry);
-
-            carry = splitNum[0];
-            lines[i].unshift(splitNum[1]);
-            unprocessedMultiplier = unprocessedMultiplier.slice(0, unprocessedMultiplier.length - 1);
-        }
-
-        if (carry !== 0) {
-            lines[i].unshift(carry);
-        }
-
-        multiplicand = multiplicand.slice(0, multiplicand.length - 1);
-    }
+            return nextLine;
+        })    
 
     let carry = 0;
     let result = [];
-    let mostCols = lines.reduce((acc, line) => {
-        return acc < line.length ? line.length : acc;
-    }, 0);
 
-    lines = lines.map((line) => {
-        let numFillSpaces = mostCols - line.length;
-        return Array(numFillSpaces).fill(0).concat(line);
-    });
+    lines = fillWithZeros(lines);
+    // TODO: invert matrix here to make sum easier and get rid of for loop
 
-    for (let col = mostCols - 1; col >= 0; col--) {
-        let sumOfCol = carry;
-
-        for (let line = 0; line < lines.length; line++) {
-            let val = lines[line][col];
-            if (val !== undefined) {
-                sumOfCol = sumOfCol + lines[line][col];
-            }
-        }
+    for (let col = lines[0].length - 1; col >= 0; col--) {
+        let sumOfCol = lines
+            .map((line) => {
+                return line[col];
+            })
+            .reduce((acc, x) => acc + x, carry);
         
         let splitCarry = splitNumAtLeastSignificantDigit(sumOfCol);
         carry = splitCarry[0];
@@ -64,6 +57,20 @@ function splitNumAtLeastSignificantDigit(num) {
     let leastSigDigit = num - rounded;
     let carry = rounded / 10;
     return [carry, leastSigDigit];
+}
+
+function fillWithZeros(multiDimArray) { 
+    let mostCols = getLongestArray(multiDimArray);
+    return multiDimArray.map((line) => {
+        let numFillSpaces = mostCols - line.length;
+        return Array(numFillSpaces).fill(0).concat(line);
+    });
+}
+
+function getLongestArray(multiDimArray) {
+    return multiDimArray.reduce((acc, array) => {
+        return acc < array.length ? array.length : acc;
+    }, 0);
 }
 
 function getLastDigit(str) {
