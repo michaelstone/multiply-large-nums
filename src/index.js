7abe25e9453559f366908428b6445e9d3e0f923e
michael stone <michaelstone.js@gmail.com>
2017-10-28 22:43:29 +0100

    Ok, now I'm done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 
-function multiplyStr(multiplier, multiplicand) {
+function multiplyStr(multiplierStr, multiplicandStr) {
+    const multiplier = stringToNumArray(multiplierStr);
 
-    return multiplicand
-        .split('')
-        .reverse()
-        .map(x => parseInt(x))
+    return stringToNumArray(multiplicandStr)
         .reduce((acc, multiplicandDigit, multiplicandIndex) => {
-            multiplier.split('')
-                .reverse()
-                .map(x => parseInt(x))
-                .forEach((multiplierDigit, multiplierIndex) => {
-                    let product = (multiplicandDigit * multiplierDigit) + ifExists(acc[multiplierIndex + multiplicandIndex], 0);
-                    let leastSigDigit = product % 10;
-                    acc[multiplierIndex + multiplicandIndex] = leastSigDigit;
-                    let carry =  Math.floor(product / 10);
-                    if (carry !== 0) {
-                        acc[multiplierIndex + multiplicandIndex + 1] = ifExists(acc[multiplierIndex + multiplicandIndex + 1], 0) + Math.floor(product / 10);
-                    }
-                });
+            multiplier.forEach((multiplierDigit, multiplierIndex) => {
+                let product = (multiplicandDigit * multiplierDigit) + ifExists(acc[multiplierIndex + multiplicandIndex], 0);
+                let leastSigDigit = product % 10;
+                acc[multiplierIndex + multiplicandIndex] = leastSigDigit;
+                let carry =  Math.floor(product / 10);
+                if (carry !== 0) {
+                    acc[multiplierIndex + multiplicandIndex + 1] = ifExists(acc[multiplierIndex + multiplicandIndex + 1], 0) + Math.floor(product / 10);
+                }
+            });
             return acc;
         }, [])
         .reverse()
         .join('');
+}
+
+function stringToNumArray(string) {
+    return string
+        .split('')
+        .reverse()
+        .map(x => parseInt(x));
 }
 
 function ifExists(value, defaultVal) {
